ac47a2ed1ca729d0c6eb0e352095c8abe75c14b8
Paulo Borges <r_pauloborges@jfrn.jus.br>
2024-10-02 15:32:22 -0300

    feat: Projeto iniciado com spring

---
+++ src/main/java/br/ufrn/DASH/DashApplication.java
@@ -0,0 +1,14 @@
+package br.ufrn.DASH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(DashApplication.class, args);
+	}
+
+}

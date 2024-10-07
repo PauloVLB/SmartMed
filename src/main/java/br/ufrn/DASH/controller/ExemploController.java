ccc9a28913aa13c9843439c64e017a2751f5eae5
Paulo Borges <r_pauloborges@jfrn.jus.br>
2024-10-07 01:29:26 +0000

    fix: adiciona id na entidade de Exemplo

--- src/main/java/br/ufrn/DASH/controller/ExemploController.java
+++ src/main/java/br/ufrn/DASH/controller/ExemploController.java
@@ -1,16 +1,24 @@
 package br.ufrn.DASH.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.ufrn.DASH.model.ExemploModel;
+import br.ufrn.DASH.repository.ExemploRepository;
 
 
 @RestController
 @RequestMapping("/exemplo")
 public class ExemploController {
+    @Autowired
+    private ExemploRepository exemploRepository;
 
     @GetMapping
-    public String exemplo() {
-        return "Hello, World!";
+    public ResponseEntity<ExemploModel> exemplo() {
+        ExemploModel exemploModel = new ExemploModel();
+        return ResponseEntity.ok(exemploRepository.save(exemploModel));
     }
 }

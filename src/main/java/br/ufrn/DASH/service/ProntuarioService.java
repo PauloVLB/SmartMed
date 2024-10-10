de17f6c6b6f940ac143fc928f5deada9f34590ca
Paulo Borges <r_pauloborges@jfrn.jus.br>
2024-10-10 11:57:17 -0300

    WIP: Endpoint para adicionar seção a um prontuario

--- src/main/java/br/ufrn/DASH/service/ProntuarioService.java
+++ src/main/java/br/ufrn/DASH/service/ProntuarioService.java
@@ -1,10 +1,12 @@
 package br.ufrn.DASH.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.ufrn.DASH.model.Prontuario;
+import br.ufrn.DASH.model.Secao;
 import br.ufrn.DASH.repository.ProntuarioRepository;
 
 @Service
@@ -47,6 +49,29 @@ public class ProntuarioService {
 
     public void deleteAll() {
         prontuarioRepository.deleteAll();
+    }
+
+    public Secao addSecao(Long idProntuario, Secao secaoNova) {
+        Prontuario prontuario = this.getById(idProntuario);
+        
+        if (prontuario == null) {
+            return null;
+        }
+        
+        if(prontuario.getSecoes() == null) {
+            prontuario.setSecoes(new ArrayList<Secao>());
+        }
+        secaoNova.setOrdem(prontuario.getSecoes().size());
+        secaoNova.setNivel(1);
+        secaoNova.setProntuario(prontuario);
+
+        prontuario.getSecoes().add(secaoNova);
+
+        prontuarioRepository.save(prontuario);
+
+        Prontuario prontuarioAtualizado = this.getById(idProntuario);
+
+        return prontuarioAtualizado.getSecoes().get(prontuarioAtualizado.getSecoes().size() - 1);
     }
 
 }

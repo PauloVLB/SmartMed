667c0882a5a380764ad09274fdbe942f37ec286b
Dante Augusto <danteaugusto.10@gmail.com>
2024-10-12 14:28:49 -0300

    fix: import certo para o RequestBody em RespostaController

--- src/main/java/br/ufrn/DASH/controller/RespostaController.java
+++ src/main/java/br/ufrn/DASH/controller/RespostaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufrn.DASH.mapper.resposta.RespostaCreate;
@@ -19,7 +20,6 @@ import br.ufrn.DASH.mapper.resposta.RespostaOutput;
 import br.ufrn.DASH.mapper.resposta.RespostaUpdate;
 import br.ufrn.DASH.model.Resposta;
 import br.ufrn.DASH.service.RespostaService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/resposta")

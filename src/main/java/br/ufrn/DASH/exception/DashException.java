c438de955d826986da166e51486c09c14aa9d945
Paulo Borges <r_pauloborges@jfrn.jus.br>
2024-10-16 11:19:29 -0300

    Revert "WIP: adiciona GlobalExcepitonHandler e EntityNotFoundException"
    This reverts commit 2fa724875e9b3690bdb73a2a67159e455b3a600b.

--- src/main/java/br/ufrn/DASH/exception/DashException.java
+++
@@ -1,9 +0,0 @@
-package br.ufrn.DASH.exception;
-
-public class DashException extends RuntimeException {
-
-    public DashException() {}
-    public DashException(Throwable cause) {
-        super(cause);
-    }
-}

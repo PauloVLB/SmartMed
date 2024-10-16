2fa724875e9b3690bdb73a2a67159e455b3a600b
Paulo Borges <r_pauloborges@jfrn.jus.br>
2024-10-16 11:19:13 -0300

    WIP: adiciona GlobalExcepitonHandler e EntityNotFoundException

---
+++ src/main/java/br/ufrn/DASH/exception/DashException.java
@@ -0,0 +1,9 @@
+package br.ufrn.DASH.exception;
+
+public class DashException extends RuntimeException {
+
+    public DashException() {}
+    public DashException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/br/ufrn/DASH/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package br.ufrn.DASH.exception;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleEntityNotFoundException(EntityNotFoundException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+        errorResponse.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
+        errorResponse.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
